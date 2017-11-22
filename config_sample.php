74088c33c7abc78c93a466144d3c3fce54b3a0a0
Masaki <masaki.kawabata@gmail.com>
2017-11-22 09:13:39 -0200

    added config to  gitignore

---
+++ config_sample.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Script to add or remove symlinks (only linux) for the folowing files
+ * header.txt -> _header.txt
+ * footer.txt -> _footer.txt
+ * template.txt -> _template.txt
+ *
+ * It searchs on all subfolders for the given path
+ *
+ * Theese links enable create and edit via dokuwiki all these files usefull for
+ * headerfooter plugin and template.
+ *
+ * Copy this file to config.php and adjust the path below
+ *
+ */
+
+$path = '../test';
+
+?>
